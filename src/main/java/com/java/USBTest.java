149ab5377c46bba217c5b78e5d16532f2f35fcec
cxz <cxz@qq.com>
2022-12-05 22:35:27 +0800

    first commit

---
+++ src/main/java/com/java/USBTest.java
@@ -0,0 +1,66 @@
+package com.java;
+
+import sun.reflect.generics.tree.VoidDescriptor;
+
+public class USBTest {
+    public static void main(String[] args) {
+        Computer com = new Computer();
+        com.transferData(new Flash());
+        //USB usb = new Flash();
+        //com.transferData(usb);
+
+
+
+
+    }
+
+
+
+        public static class Computer{
+
+                public void transferData(USB usb){
+                    usb.start();
+
+                    System.out.println("---------------");
+
+                    usb.stop();
+                }
+            }
+
+
+
+    }
+
+
+    interface USB{
+        void start();
+        void stop();
+    }
+
+    class Flash implements USB{
+        @Override
+        public void start() {
+            System.out.println("U盘开始工作");
+        }
+        @Override
+        public void stop() {
+            System.out.println("U盘停止工作");
+        }
+    }
+
+    class printer implements USB{
+
+        @Override
+        public void start() {
+            System.out.println("打印机开始工作");
+
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("打印机停止工作");
+        }
+    }
+
+
+
